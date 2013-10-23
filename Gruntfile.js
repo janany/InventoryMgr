c458ba78453f65d66baafdc6aa6e46573d17955f
Janani <jananyjayaraman@gmail.com>
2013-10-23 15:08:57 +0530

    Grunt: copy files to destination

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,17 +15,44 @@ module.exports = function(grunt) {
 	},
 
     assetbuild: {
-        options: {
-            moduleBaseUrl: 'app'
+        generate:{
+            options: {
+                moduleBaseUrl: 'app',
+                baseJson: 'mortar.json',
+                baseFile: '_index.html'
+            }
+        }
+    },
 
+    copy: {
+        css:{
+            files: [
+                {
+                    expand: true,
+                    flatten: true,
+                    cwd: 'app',
+                    src: ['lib/css/*.css'],
+                    dest: 'app/dist/styles'
+                }
+            ]
         },
-        baseFile: 'app/_index.html'
+        images: {
+            files: [
+                {
+                    expand: true,
+                    cwd: 'app',
+                    src: ['images/*.{gif,png,jpg,jpeg}'],
+                    dest: 'app/dist'
+                }
+            ]
+        }
     }
     });
 
     // Load required modules
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks("grunt-assetbuild");
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
 };
 
